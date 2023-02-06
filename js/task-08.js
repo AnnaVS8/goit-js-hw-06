c93aeba42cb6901c95d5812859f58a9ca93748a6
Anna Sibova <anna_sibova@ukr.net>
2023-02-06 22:04:01 +0200

    add other

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const login = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  const formData = {
+    login,
+    password,
+  };
+  login && password !== "" ? console.log(formData) : alert("Відсутні дані");
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  button: document.querySelector(".change-color"),
+  BodyColor: document.querySelector(".color"),
+};
+// console.log(refs.button);
+// console.log(refs.BodyColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function randomColorOnBody() {
+  document.body.style.background = getRandomHexColor();
+  refs.BodyColor.textContent = getRandomHexColor();
+}
+
+refs.button.addEventListener("click", randomColorOnBody);
